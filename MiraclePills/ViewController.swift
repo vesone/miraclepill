1297c0c9a84b06e5e53fcaf8089405f892f5d486
vesone <vesonedean@gmail.com>
2016-07-29 16:30:15 -0400

    added an image

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red()
     }
 
     override func didReceiveMemoryWarning() {
