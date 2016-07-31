580802f3b728fe8c30fb73690df13ea53d012b19
vesone <vesonedean@gmail.com>
2016-07-31 13:34:51 -0400

    State picker and state button changes

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var statePicker: UIPickerView!
     @IBOutlet weak var statePickerBtn: UIButton!
     
+    let states = ["Alaska", "Kansas", "California", "Louisiana", "Indiana", "Florida"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         statePicker.dataSource = self
@@ -25,6 +27,28 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
 
     @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        
+        statePicker.isHidden = false
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        
+//        statePicker.isHidden = true
         
     }
 
